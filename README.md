938bd8cbfbae349cf72224662558c82d3e65fbf3
yuvraj2009bhatnagar <73655765+yuvraj2009bhatnagar@users.noreply.github.com>
2021-05-08 18:17:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# tower
+# TowerSiege-1
---
+++ Slingshot.js
@@ -0,0 +1,30 @@
+class Slingshot{
+
+    constructor(bodyA,pointB){
+    
+        var options = {
+    
+            bodyA:bodyA ,
+            bodyB: pointB,
+            stiffness: 0.04,
+            length: 10
+            
+            
+                }
+
+                this.pointB = pointB
+    
+                this.sling = Constraint.create(options)
+                World.add(world,this.sling)
+    
+    
+    
+    }
+    display(){
+        if(this.sling.bodyA){
+    var pointA = this.sling.bodyA.position
+    var pointB = this.pointB
+    stroke("white")
+    strokeWeight(4)
+        line(pointA.x,pointA.y,pointB.x,pointB.y)}
+    }}
